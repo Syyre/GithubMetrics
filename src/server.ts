00d362fbc6d0881af8518e03d3d71fc902ecc07d
Mathu <z5419643@ad.unsw.edu.au>
2026-09-15 13:41:04 +1000

    added fetching username data

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,26 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
-console.log(`Server is running on http://localhost:${PORT}`);
+app.get("/api/github/:username", async (req: Request, res: Response) => {
+  const username = req.params.username;
 
-app.listen(PORT);
+  const response = await fetch(`https://api.github.com/users/${username}`);
+
+  const data = await response.json();
+
+  res.json({
+    username: data.login,
+    name: data.name,
+    bio: data.bio,
+    public_repos: data.public_repos,
+    followers: data.followers,
+    following: data.following,
+    created_at: data.created_at,
+    email: data.email,
+    location: data.location,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
